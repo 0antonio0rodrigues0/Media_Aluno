2c4da204ae0a4172f4c478c08a42fe5e6c8f134c
Antonio Rodrigues <0antonio0rodrigues0>
2024-06-18 07:50:24 -0300

    dshgfj

--- ExemploNotas/src/main/java/com/mycompany/exemplonotas/ExemploNotas.java
+++ ExemploNotas/src/main/java/com/mycompany/exemplonotas/ExemploNotas.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.exemplonotas;
 
+import java.util.Scanner;
+
 
 
 /**
@@ -11,37 +13,37 @@ package com.mycompany.exemplonotas;
  * @author antro
  */
 public class ExemploNotas {
-    /*Alunos aprovados:
-Aluno 1 aprovado 7.0
-Aluno 2 aprovado 7.0
-Aluno 3 aprovado 7.0
-Aluno 4 aprovado 7.0*/
 
     public static void main(String[] args) {
-        double notas = 7; 
-        double notaAluno1 = notas;
-        double notaAluno2 = notas;
-        double notaAluno3 = notas;
-        double notaAluno4 = notas;
-        if(notas <= 7 ){
-        System.out.println("Alunos aprovados:");
-        System.out.println("Aluno 1 aprovado " +notaAluno1);
-        System.out.println("Aluno 2 aprovado " +notaAluno2);
-        System.out.println("Aluno 3 aprovado " +notaAluno3);
-        System.out.println("Aluno 4 aprovado " +notaAluno4);
+        Scanner n = new Scanner(System.in);
+        double notas = 7;
+        
+        System.out.println("Digite sua nota:");
+        
+        notas = n.nextInt();
+    
+        if(notas >= 7 ){
+        System.out.println("Aprovado");
         
         }
-        else if(notas < 5){
-        System.out.println("Recuperação");
-                
-          }
+        
+        
+        else if(notas <= 5 || notas < 7){
+        System.out.println("De recuperação");
+        
+        }
+        
         else{
         System.out.println("Reprovado");
+        
         }
+        
+      
+    } 
        
-    }
 }
-          
+
+      
          
     
 
